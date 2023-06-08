3064344ddc1265facc9c63a0469bae4f0c7b67a3
Viktor Kodet <viktor.kodet@skandia.se>
2023-06-08 14:56:36 +0200

    claimstatus migration

--- SkeppOHoj/Migrations/20230608125528_claimstatus.Designer.cs
+++ SkeppOHoj/Migrations/20230608125528_claimstatus.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkeppOHoj.Data;
 
@@ -11,9 +12,11 @@ using SkeppOHoj.Data;
 namespace SkeppOHoj.Migrations
 {
     [DbContext(typeof(SkeppOHojContext))]
-    partial class SkeppOHojContextModelSnapshot : ModelSnapshot
+    [Migration("20230608125528_claimstatus")]
+    partial class claimstatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SkeppOHoj/Migrations/20230608125528_claimstatus.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkeppOHoj.Migrations
+{
+    /// <inheritdoc />
+    public partial class claimstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Status",
+                table: "Status");
+
+            migrationBuilder.RenameTable(
+                name: "Status",
+                newName: "ClaimStatus");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "InsuranceType",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClaimStatus",
+                table: "ClaimStatus",
+                column: "ClaimStatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClaimStatus",
+                table: "ClaimStatus");
+
+            migrationBuilder.RenameTable(
+                name: "ClaimStatus",
+                newName: "Status");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "InsuranceType",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Status",
+                table: "Status",
+                column: "ClaimStatusId");
+        }
+    }
+}
